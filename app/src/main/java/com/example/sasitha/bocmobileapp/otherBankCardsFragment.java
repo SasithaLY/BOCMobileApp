65b2d286b6ea0e3cac5b7c057c6ea670daf73cc7
it17163132@my.sliit.lk <it17163132@my.sliit.lk>
2019-09-10 00:12:20 +0530

    Pasana Latest

--- app/src/main/java/com/example/sasitha/bocmobileapp/otherBankCardsFragment.java
+++ app/src/main/java/com/example/sasitha/bocmobileapp/otherBankCardsFragment.java
@@ -1,6 +1,9 @@
 package com.example.sasitha.bocmobileapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,11 +12,17 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class otherBankCardsFragment extends Fragment
 {
     Button btnOtherBankSubmit, btnOtherBankCancel;
+    EditText editTextOtherCardNo, editTextOtherName, editTextOtherNickName, editTextOtherMail;
+
+    Vibrator vib;
+    Animation animShake;
 
     Fragment fragment = null;
     FragmentManager fragmentManager;
@@ -32,6 +41,11 @@ public class otherBankCardsFragment extends Fragment
         btnOtherBankCancel = (Button) view.findViewById(R.id.btnOtherBocCancel);
         btnOtherBankSubmit = (Button) view.findViewById(R.id.btnOtherBocSubmit);
 
+        editTextOtherCardNo = (EditText)  view.findViewById(R.id.editTextOtherCardNo);
+        editTextOtherName = (EditText)  view.findViewById(R.id.editTextOtherName);
+        editTextOtherNickName = (EditText)  view.findViewById(R.id.editTextOtherNickName);
+        editTextOtherMail = (EditText)  view.findViewById(R.id.editTextOtherMail);
+
         fragmentManager = getFragmentManager();
 
         btnOtherBankCancel.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +57,74 @@ public class otherBankCardsFragment extends Fragment
                 fragmentTransaction.replace(R.id.screen_area, fragment);
                 fragmentTransaction.commit();
                 getActivity().setTitle("Add Accounts");
+            }
+        });
+
+        btnOtherBankSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                if(editTextOtherCardNo.getText().toString().isEmpty())
+                {
+                    editTextOtherCardNo.setError("Enter Card Number!");
+                    editTextOtherCardNo.startAnimation(animShake);
+                    vib.vibrate(120);
+                }
+                else if(editTextOtherName.getText().toString().isEmpty())
+                {
+                    editTextOtherName.setError("Enter Name!");
+                    editTextOtherName.startAnimation(animShake);
+                    vib.vibrate(120);
+                }
+                else if(editTextOtherNickName.getText().toString().isEmpty())
+                {
+                    editTextOtherNickName.setError("Enter Nick Name!");
+                    editTextOtherNickName.startAnimation(animShake);
+                    vib.vibrate(120);
+                }
+                else if(editTextOtherMail.getText().toString().isEmpty())
+                {
+                    editTextOtherMail.setError("Enter E-Mail!");
+                    editTextOtherMail.startAnimation(animShake);
+                    vib.vibrate(120);
+                }
+                else{
+                    fragment = new addAccountFragment();
+                    fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.screen_area, fragment);
+                    fragmentTransaction.commit();
+                    getActivity().setTitle("Add Accounts");
+                }
+            }
+        });
+
+        btnOtherBankCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Confirm!");
+                builder.setMessage("Are you sure you want to cancel?");
+                builder.setCancelable(false);
+
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        Fragment fragmentD = new thirdPartyFragment();
+                        fragmentTransaction.replace(R.id.screen_area, fragmentD);
+                        fragmentTransaction.commit();
+                        getActivity().setTitle("Third Party Transfers");
+                    }
+                });
+
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                builder.show();
             }
         });
 
---
+++ app/src/main/java/com/example/sasitha/bocmobileapp/transferMoneyFragment.java
@@ -0,0 +1,130 @@
+package com.example.sasitha.bocmobileapp;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class transferMoneyFragment extends Fragment
+{
+    TextView textViewtransferFromAccNo, textViewTransferToAccNo;
+    EditText editTextAmount, editTextDescription;
+    Button btnTransferCancel, btnTransfer;
+
+    Fragment fragment = null;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
+    Vibrator vib;
+    Animation animShake;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        animShake = AnimationUtils.loadAnimation(getContext(), R.anim.shake);
+        vib = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+
+        textViewtransferFromAccNo = (TextView)view.findViewById(R.id.textViewtransferFromAccNo);
+        textViewTransferToAccNo = (TextView)view.findViewById(R.id.textViewTransferToAccNo);
+
+        editTextAmount = (EditText) view.findViewById(R.id.editTextAmount);
+        editTextDescription = (EditText) view.findViewById(R.id.editTextDescription);
+
+        btnTransferCancel = (Button) view.findViewById(R.id.btnTransferCancel);
+        btnTransfer = (Button) view.findViewById(R.id.btnTransfer);
+
+        //Bundle bundle = getArguments();
+
+        fragmentManager = getFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        btnTransfer.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Bundle bundle = new Bundle();
+                bundle.putString("tranFromAcc", textViewtransferFromAccNo.getText().toString());
+                bundle.putString("tranToAcc", textViewTransferToAccNo.getText().toString());
+
+                if(editTextAmount.getText().toString().isEmpty())
+                {
+                    editTextAmount.setError("Enter amount!");
+                    editTextAmount.startAnimation(animShake);
+                    vib.vibrate(120);
+                }
+                else if(editTextDescription.getText().toString().isEmpty())
+                {
+                    editTextDescription.setError("Enter Description!");
+                    editTextDescription.startAnimation(animShake);
+                    vib.vibrate(120);
+                }
+                else
+                {
+                    double d = Double.parseDouble(editTextAmount.getText().toString());
+                    bundle.putString("transAmount" , String.format(Locale.getDefault(),"%.2f", d));
+                    bundle.putString("tranDesc", editTextDescription.getText().toString());
+
+                    fragment.setArguments(bundle);
+                    //fragment = new OwnAccountConfirmFragment();
+                    fragmentTransaction.replace(R.id.screen_area, fragment);
+                    fragmentTransaction.commit();
+                }
+            }
+        });
+
+        btnTransferCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Confirm!");
+                builder.setMessage("Are you sure you want to cancel?");
+                builder.setCancelable(false);
+
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        Fragment fragmentD = new DashboardFragment();
+                        fragmentTransaction.replace(R.id.screen_area, fragmentD);
+                        fragmentTransaction.commit();
+                        getActivity().setTitle("Home");
+                    }
+                });
+
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                builder.show();
+            }
+        });
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_transfer_money, null);
+    }
+}
